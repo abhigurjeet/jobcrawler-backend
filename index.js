a6528660a888caae59a38917c16c67d746df1057
Gurjeet Singh <abhigurjeet@gmail.com>
2025-08-27 22:11:06 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 
 const app =express();
-
+app.use(cors({
+    origin: "http://localhost:5173"
+  }));
 app.get('/',async(req,res)=>{
     res.send('HELLO FROM BACKEND');
 })
