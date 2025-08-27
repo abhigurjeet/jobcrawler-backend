5411108adb0fdaa436e8114ab1af3803e93e8e35
Gurjeet Singh <abhigurjeet@gmail.com>
2025-08-27 22:23:30 +0530

    allow all origins

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const cors = require('cors')
 
 const app =express();
-app.use(cors({
-    origin: "http://localhost:5173"
-  }));
+app.use(cors());
 app.get('/',async(req,res)=>{
     res.send('HELLO FROM BACKEND');
 })
